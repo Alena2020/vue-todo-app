da2f6a67275bca641c3f4ddb605d3e8359cfadd5
Alena <elenila_skinef@mail.ru>
2023-02-22 19:11:57 +0300

    Validation of parameters

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,3 +1,21 @@
 <template>
-  <li>Todo item 1</li>
+  <li>
+    <span>
+      <input type="checkbox" />
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+    <button>&times;</button>
+  </li>
 </template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
